efaeb38c4598affba2e4a6d0cae5b71af6c68a47
karsyn ansari <karsyn.ansari@gmail.com>
2022-03-22 13:42:40 -0600

    commit tree 7b19dc8f0d7ab957e71cc648e08abd1453d5a556

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
+session[:page_views]  ||= 0
+session[:page_views] += 1
+if session[:page_views] <= 3
+article = Article.find(params[:id])
     render json: article
+else 
+render json: {error: "Maximum pageview limit reached"}, status: 401 
   end
-
+end 
   private
 
   def record_not_found
